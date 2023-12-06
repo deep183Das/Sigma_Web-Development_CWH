29d59f1669aaa8729224ba364c452214917d991e
Deepanjhan Das <deepanjhan0206@gmail.com>
2023-12-06 10:47:16 +0530

    Till Tut54

---
+++ Tut54 _ JS/script.js
@@ -0,0 +1,30 @@
+
+    // giving the alert message 
+// alert("Hello World");
+
+    // printing the message in the console
+// console.log("Code is running...");
+    // using this messages we can check upto which point our code has reached and make our path of debugging easier
+
+    // prompt feature in JS and we can check in the console that a gets which value
+// var a = prompt("Enter your number :)")
+
+    // concatenation in JS
+// console.log("Your number is " + a)
+
+    // confirm
+// var b = confirm("Are you sure you want to leave this page and blast your computer ? :(")
+// if(b){
+//     alert("Blasting your computer!")
+//     // first the alert message will be shown and then the following message will come on the console
+//     console.log("Your computer is blasting!")
+//     alert("Just Kidding :)")
+// } else {
+//     console.log("Your computer is not blasting!")
+// }
+
+    // using JS styling the webpage
+document.title = "Hey, this is the intro to JS"
+
+    // the following code will add the inline css to the body
+document.body.style.backgroundColor = "yellow"
