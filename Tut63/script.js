f389fc2546fdf83298b37488287dc03823c807ec
Deepanjhan Das <deepanjhan0206@gmail.com>
2023-12-16 12:31:50 +0530

    Till Tut65

---
+++ Tut63/script.js
@@ -0,0 +1,45 @@
+let arr = [1, 2, 4, 5, 7]
+// Index   0  1  2  3  4
+
+// console.log(arr);
+// console.log(arr.length);
+// console.log(arr[0])
+// console.log(arr[4])
+    // arrays are mutable (where strings are immutable)
+// arr[0] = 20;
+// console.log(arr)
+
+// console.log(typeof arr)
+    // JS arrays are objects 
+
+    // array methods
+// console.log(arr.toString());    // it will convert into a comma separated string
+// console.log(arr.join(" and ")); 
+
+// a = [1, 2, 3, 4, 5, 6]
+// console.log(a.pop());       // returns the popped element from the back and array get changed
+// console.log(a)
+
+// console.log(a.push("Deep")) // modifies the array and returns the new array length
+// console.log(a)
+
+// console.log(a.shift());     // return the removed element from the first
+// console.log(a)
+
+// console.log(a.unshift("Momo")); // pushes new element from the front and returns the length of the modified array
+// console.log(a)
+
+// delete a[5];        // this doesn't change the array length
+// console.log(a);     
+// console.log(a.length);     
+// console.log(a[5])   // undefined
+
+// b = ["Deep", "Momo"]
+// c = ["H", "W"]
+// console.log(a.concat(b, c));    // returns a new array and doesn't change the existing arrays
+
+
+
+/*
+    take references from the notes of Tut16 to 21 of JS course tutorials
+*/
